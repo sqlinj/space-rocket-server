54f726a69be52b1f62d030621257bf547125f4f2
Vörös Bence <bencee.voros001@gmail.com>
2019-02-27 13:47:29 +0100

    Create encode

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ io.on('connection', (client) => {
   client.on('subscribeToTimer', (interval) => {
     console.log('client is subscribing to timer with interval ', interval);
     setInterval(() => {
-      client.emit('timer', Math.random());
+      client.emit('timer', new Date());
     }, interval);
   });
+
+  client.on('encodeData', (data) => {
+    console.log('client is encode the ', data);
+    const result = new Buffer(data).toString('base64');
+    client.emit('encoder', result);
+  })
 });
 
 const port = process.env.PORT || 8000;
