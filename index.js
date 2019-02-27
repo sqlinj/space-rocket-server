9ec56bd57b3be340eed9076f21f63a9a13acef00
Vörös Bence <bencee.voros001@gmail.com>
2019-02-27 12:28:04 +0100

    Initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const io = require('socket.io')();
+
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', Math.random());
+    }, interval);
+  });
+});
+
+const port = process.env.PORT || 8000;
+io.listen(port);
+console.log('listening on port ', port);
